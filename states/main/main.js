bd485add49a73bbaa3acb823478b30ed250f4725
Luke Dobinson <ldobinson90@gmail.com>
2016-09-18 12:21:34 +0100

    Initial commit.

---
+++ states/main/main.js
@@ -0,0 +1,18 @@
+var LightsOut = (function(lightsOut){
+  lightsOut.States = lightsOut.States || {};
+  lightsOut.States.Main = {
+    preload: function() {
+      // load assets.  
+    },
+
+    create: function() {
+      // spawn sprites.
+    },
+
+    update: function() {
+      // game logic.
+    },
+  };
+
+  return LightsOut;
+}(LightsOut || {}));
---
+++ states/menu/menu.js
@@ -0,0 +1,30 @@
+var LightsOut = (function(lightsOut){
+  lightsOut.States = lightsOut.States || {};
+
+  lightsOut.States.Menu = {
+    preload: function () {
+
+    },
+
+    create: function () {
+      game.stage.backgroundColor = '#20343B';
+
+      this.title = game.add.text(game.world.centerX, game.world.centerY, 
+        'WASD to move, and E to interact', { font: '20px monospace', fill: "white" });
+      this.title.anchor.set(0.5, 0.5);
+
+      this.subtitle = game.add.text(game.world.centerX, game.world.centerY + 80,
+        'Press any key for New Game', { font: '18px monospace', fill: "white" });
+      this.subtitle.anchor.set(0.5, 0.5);
+
+      game.input.keyboard.onDownCallback = function () {
+        game.state.start('main', true, true);
+        game.input.keyboard.onDownCallback = null;
+      };
+    },
+
+    update: function () { }
+  };
+
+  return lightsOut;
+}(LightsOut || {}));
