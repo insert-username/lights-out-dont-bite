7557322e5cb1f11973962e1d43118249bcb90369
Luke Dobinson <ldobinson90@gmail.com>
2016-09-18 15:32:10 +0100

    Added lighting state to room.

--- states/main/entities/room.js
+++ states/main/entities/room.js
@@ -1,3 +1,8 @@
+/**
+ * This file defines the room for the game.
+ * A room consists of a tiled floor, several walls to block player entry, and a lighting state.
+ * Rooms may be either lit or unlit.
+ */
 var LightsOut = (function(lightsOut){
 
   lightsOut.Room = function(game, x, y, width, height) {
@@ -31,7 +36,43 @@ var LightsOut = (function(lightsOut){
     this.group.add(lightsOut.Room.makeHWallLeft(game, width, roomLeft, roomBottom - lightsOut.Room.wallWidth, wallPC));
     this.group.add(lightsOut.Room.makeHWallRight(game, width, roomLeft, roomBottom - lightsOut.Room.wallWidth, wallPC));
 
+    /** 
+     * The lighting for the room sets the level of visibility, an alpha value of 1.0 indicates an unlit room, 
+     * and a value of 0.0 indicates full visibility.
+     */
+    this.lighting = game.add.tileSprite(roomLeft, roomTop, width, height, lightsOut.Room.lightingKey);
+    this.group.add(this.lighting);
+
     this.group.sort();
+
+    /**
+     * The min and max alpha values for the lighting in this room.
+     */
+    this.lightingAlphaMin = 1.0;
+    this.lightingAlphaMax = 1.0;
+    this.lightingFlickerPeriodSeconds = 5;
+    game.time.events.loop(Phaser.Timer.SECOND * this.lightingFlickerPeriodSeconds / 50, this.updateLightingAlpha, this);
+    game.time.events.start();
+    this.setState(game, lightsOut.Room.State.LIT, 1000);
+  };
+
+  /**
+   * Describes the lighting values of the room.
+   * The values for the enum keys are the max alpha
+   * the lighting element should have when the room
+   * is in that state. 
+   */
+  lightsOut.Room.State = {
+
+    /**
+     * Indicates that the room is currently illuminated.
+     */
+    LIT : 0.8,
+
+    /**
+     * Indicates that the room is dark.
+     */
+    UNLIT : 1.0
   };
 
   /** Factory function for creating a room group. */
@@ -43,10 +84,12 @@ var LightsOut = (function(lightsOut){
    * Unique key by which room resources may be referenced.
    */
   lightsOut.Room.key = "room";
+  lightsOut.Room.lightingKey = "lighting";
   lightsOut.Room.wallKey = "wall";
   lightsOut.Room.load = function(game) {
     game.load.spritesheet(lightsOut.Room.key, "assets/sprites/room.png", 50, 50);
     game.load.spritesheet(lightsOut.Room.wallKey, "assets/sprites/wall.png", 10, 10);
+    game.load.spritesheet(lightsOut.Room.lightingKey, "assets/sprites/lighting.png", 10, 10);
   };
 
   lightsOut.Room.wallWidth = 10;
@@ -103,6 +146,37 @@ var LightsOut = (function(lightsOut){
    */
   lightsOut.Room.prototype.collideWith = function(sprite) {
     this.game.physics.arcade.collide(this.group, sprite);
+  };
+
+  /**
+   * Illuminates the room.
+   */
+  lightsOut.Room.prototype.setLit = function(game) {
+    this.setState(game, lightsOut.Room.State.LIT, 1000);
+  };
+
+  /**
+   * Sets the room to dark.
+   */
+  lightsOut.Room.prototype.setUnlit = function(game) {
+    this.setState(game, lightsOut.Room.State.UNLIT, 1000);
+  };
+
+  /**
+   * @param State must be one of lightsOut.Room.State.
+   */
+  lightsOut.Room.prototype.setState = function(game, state, tweenTimeMs) {
+    this.state = state;
+
+    var lightingTween = game.add.tween(this);
+    lightingTween.to( { lightingAlphaMin: state * 0.9, lightingAlphaMax: state }, tweenTimeMs, Phaser.Easing.Linear.None);
+    lightingTween.start();
+  };
+
+  lightsOut.Room.prototype.updateLightingAlpha = function() {
+    var time = this.game.time.totalElapsedSeconds();
+    var lightingLerp = (Math.sin(2 * Math.PI * time / this.lightingFlickerPeriodSeconds) + 1) / 2;
+    this.lighting.alpha = this.lightingAlphaMin + (this.lightingAlphaMax - this.lightingAlphaMin) * lightingLerp;
   };
 
   return lightsOut;
