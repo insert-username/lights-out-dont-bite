f8fdc78eb53c1d8a253bad44eea7b309c4b1ec51
Luke Dobinson <ldobinson90@gmail.com>
2016-09-22 21:30:41 +0200

    Enable tilemap collisions.

--- states/main/main.js
+++ states/main/main.js
@@ -29,19 +29,24 @@ var LightsOut = (function(lightsOut){
 
       //  Creates a layer from the World1 layer in the map data.
       //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
-      layer = map.createLayer('Walls');
-
+      this.wallLayer = map.createLayer('Walls');
+      map.setCollisionBetween(1, 1);
       //  This resizes the game world to match the layer dimensions
-      layer.resizeWorld();
+      this.wallLayer.resizeWorld();
+      this.wallLayer.debug = this.debugMode;
 
       var zDepth = new lightsOut.ZDepth(this.game);
-      zDepth.wall.add(layer);
+      zDepth.wall.add(this.wallLayer);
 
       var mapImporter = new lightsOut.MapImporter(this.game, zDepth, map);
       this.roomManager = mapImporter.getRoomManager();
       this.navMesh = mapImporter.getNavMesh();
       this.player = mapImporter.getPlayer();
       this.nasty = mapImporter.getEnemy();
+
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.game.physics.arcade.enable(this.wallLayer);
+      this.game.physics.arcade.enable(this.player);
 
       var style = { font: "32px verdana", fill: "#ff0044", wordWrap: true, wordWrapWidth: 300, align: "center" };
       this.deathText = this.game.add.text(0, 0, "You Are Dead.", style);
@@ -51,7 +56,6 @@ var LightsOut = (function(lightsOut){
         this.game.camera.view.centerY);
       this.deathText.visible = false;
       this.deathText.alpha = 0;
-      this.game.physics.startSystem(Phaser.Physics.ARCADE);
     },
 
     update: function() {
@@ -59,6 +63,8 @@ var LightsOut = (function(lightsOut){
       if (!this.player.isAlive()) {
         return;
       }
+
+      this.game.physics.arcade.collide(this.player, this.wallLayer);
 
       this.roomManager.step();
       this.nasty.step(this.player);
@@ -81,6 +87,8 @@ var LightsOut = (function(lightsOut){
 
         game.debug.geom(new Phaser.Circle(this.player.x, this.player.y, 50));
         game.debug.geom(new Phaser.Circle(this.nasty.x, this.nasty.y, 50));
+
+        game.debug.bodyInfo(this.player, 10, 10);
 
         var navPoints = this.navMesh.points;
         var game = this.game;
